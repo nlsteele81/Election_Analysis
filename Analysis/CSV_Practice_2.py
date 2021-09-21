7c91ccd065a6806bea87734bbea56b7ce625b1eb
nlsteele81 <nicole_l_steele@yahoo.com>
2021-09-21 07:02:40 -0500

    Adding the election audit code.

--- Analysis/CSV_Practice_2.py
+++ Analysis/CSV_Practice_2.py
@@ -7,6 +7,20 @@ import os
 file_to_load = '/Users/nikki/Desktop/DataBootCamp/DataClasswork/Python/Election_Analysis/Analysis/election_results.csv'
 #file_to_load = os.path.join("Analysis", "election_results.csv")
 
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Declare a new list of candidate options.
+candidate_options = []
+
+# Declare an empty dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -20,11 +34,74 @@ with open(file_to_load) as election_data:
 
 # # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
     
 # # Print each row in the CSV file.
-#     for row in file_reader:
-#         print(row)
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # Add the candidate name to the candidate list.
+        #candidate_options.append(candidate_name)
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# # Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    # 4. Print the candidate name and percentage of votes + Skill Drill.
+    #print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # 2. If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+
+        # 3. Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+# To do: Print out the winning candidate, vote count and percentage to terminal.
+                    
+# 3. Print the total votes.
+#print(total_votes)
+
+# Print the candidate list.
+#print(candidate_options)
+
+# Print the candidate vote dictionary.
+#print(candidate_votes)
     
 # # Close the file.
 #election_data.close()
